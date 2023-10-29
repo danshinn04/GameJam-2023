04227af82791dcb604d6d8be07b3b8963d29686e
Charles Wang <czw@seas.upenn.edu>
2023-10-29 04:05:37 -0400

    lint file

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,95 +1,84 @@
 using UnityEngine;
 
+public enum Gun
+{
+    Pistol = 0,
+    Automatic = 1,
+    Shotgun = 2
+}
+
 public class PlayerScript : MonoBehaviour
 {
-    private float speed = 5.0f;
-    Vector3 cVector;
     public GameObject bullet;
     public GameObject pellet;
     public Rigidbody2D rb;
-
+    
     public Animator anim;
     private static readonly int IsShooting = Animator.StringToHash("isShooting");
     
-    private int gunType = 1;
+    private const float Speed = 5.0f;
+    private Gun _gun = Gun.Pistol;
+    private Vector3 _cVector = new(0, 0, 0);
 
-    // pistolCoolDown
-    private float pistolCoolDown;
+    private float _pistolCooldown;
+    private float _automaticCooldown;
+    private float _shotgunCooldown;
 
-    // shotgunCoolDown
-    private float shotgunCoolDown;
-
-    // automaticCoolDown
-    private float automaticCoolDown;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        cVector = new Vector3(0, 0, 0);
-    }
-
-    // shotgun, weapon
-    // pistol weapon
-    // rifle weapon
-
-    void rotatePlayer() {
-        Vector3 charVector = Camera.main.WorldToScreenPoint(transform.position);
-        cVector = Input.mousePosition - charVector;
-        float angle = Mathf.Atan2(cVector.y, cVector.x) * Mathf.Rad2Deg;
- 
-        //transform.position = charVector;
+    private void RotatePlayer() {
+        var charVector = Camera.main.WorldToScreenPoint(transform.position);
+        _cVector = Input.mousePosition - charVector;
+        
+        var angle = Mathf.Atan2(_cVector.y, _cVector.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
 
-    void shootPistol() {
-        if(pistolCoolDown == 0.0f) {
-            // mouseCoolDown
-            pistolCoolDown = 0.05f;
-            GameObject projectile = Instantiate(bullet, transform.position + (0.5f * cVector.normalized), transform.rotation);
-            projectile.GetComponent<BulletScript>().direction = cVector;
-            projectile.GetComponent<BulletScript>().speed = 12.0f;
-        }
+    private void ShootPistol()
+    {
+        if (_pistolCooldown != 0.0f) return;
+        
+        _pistolCooldown = 0.05f;
+        
+        var projectile = Instantiate(bullet, transform.position + (0.5f * _cVector.normalized), transform.rotation);
+        projectile.GetComponent<BulletScript>().direction = _cVector;
+        projectile.GetComponent<BulletScript>().speed = 12.0f;
     }
 
-    void shootAuto() {
-        if(automaticCoolDown == 0.0f) {
-            // mouseCoolDown
-            automaticCoolDown = 0.10f;
-            GameObject projectile = Instantiate(bullet, transform.position + (0.5f * cVector.normalized), transform.rotation);
-            projectile.GetComponent<BulletScript>().direction = cVector;
-            projectile.GetComponent<BulletScript>().speed = 12.0f;
-        }
+    private void ShootAuto()
+    {
+        if (_automaticCooldown != 0.0f) return;
+        
+        _automaticCooldown = 0.10f;
+        
+        var projectile = Instantiate(bullet, transform.position + (0.5f * _cVector.normalized), transform.rotation);
+        projectile.GetComponent<BulletScript>().direction = _cVector;
+        projectile.GetComponent<BulletScript>().speed = 12.0f;
     }
 
-    void shootShotgun() {
-        if(shotgunCoolDown == 0.0f) {
-            // mouseCoolDown
-            int pellets = 4;
-            shotgunCoolDown = 0.65f;
-            for(int i = -pellets + 1; i < pellets; i++) {
-                float angle = i / (float) pellets * 10.0f;
-                float angelRad= Mathf.Deg2Rad * angle;
+    private void ShootShotgun()
+    {
+        if (_shotgunCooldown != 0.0f) return;
+        
+        _shotgunCooldown = 0.65f;
+        var pellets = 4;
+        
+        for (var i = -pellets + 1; i < pellets; i++) {
+            var angle = i / (float) pellets * 10.0f;
                 
-                Vector2 rotatedVector = Quaternion.Euler(0, 0, angle) * cVector;
-                rotatedVector.Normalize();
+            Vector2 rotatedVector = Quaternion.Euler(0, 0, angle) * _cVector;
+            rotatedVector.Normalize();
 
-                GameObject pellet = Instantiate(bullet, transform.position + (0.5f * cVector.normalized), transform.rotation);
-                pellet.transform.localScale = new Vector3(0.175f, 0.175f, 0.0f);
-                pellet.GetComponent<BulletScript>().setDamage(15.0f);
-                pellet.GetComponent<BulletScript>().direction = rotatedVector;
-                pellet.GetComponent<BulletScript>().speed = 16.0f + Random.Range(1,5);
-            }
+            var pellet = Instantiate(bullet, transform.position + (0.5f * _cVector.normalized), transform.rotation);
+            pellet.transform.localScale = new Vector3(0.175f, 0.175f, 0.0f);
+            pellet.GetComponent<BulletScript>().setDamage(15.0f);
+            pellet.GetComponent<BulletScript>().direction = rotatedVector;
+            pellet.GetComponent<BulletScript>().speed = 16.0f + Random.Range(1,5);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         float h = 0;
         float v = 0;
-        
-        //Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //mouseWorldPos.z = 0f;
 
         if(Input.GetKey(KeyCode.W)) {
             v = 1;
@@ -104,20 +93,33 @@ public class PlayerScript : MonoBehaviour
             h = 1;
         }
 
-        rotatePlayer();
-        
-        switch (gunType)
+        if (Input.GetKey(KeyCode.Alpha1))
         {
-            case 0 when Input.GetMouseButtonDown(0):
-                shootPistol();
+            _gun = Gun.Pistol;
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            _gun = Gun.Automatic;
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            _gun = Gun.Shotgun;
+        }
+
+        RotatePlayer();
+        
+        switch (_gun)
+        {
+            case Gun.Pistol when Input.GetMouseButtonDown(0):
+                ShootPistol();
                 anim.SetBool(IsShooting, true);
                 break;
-            case 1 when Input.GetMouseButton(0):
-                shootAuto();
+            case Gun.Automatic when Input.GetMouseButton(0):
+                ShootAuto();
                 anim.SetBool(IsShooting, true);
                 break;
-            case 2 when Input.GetMouseButtonDown(0):
-                shootShotgun();
+            case Gun.Shotgun when Input.GetMouseButtonDown(0):
+                ShootShotgun();
                 anim.SetBool(IsShooting, true);
                 break;
             default:
@@ -125,27 +127,26 @@ public class PlayerScript : MonoBehaviour
                 break;
         }
 
-        Vector2 change = new Vector2(h, v).normalized;
+        var change = new Vector2(h, v).normalized;
         if(change.magnitude > 0.0f) {
             float n = GameManager.CurrentMap[0].Length;
             float m = GameManager.CurrentMap.Length;
 
-            // unity raycasting sucks ass
+            // Unity raycasting sucks ass
             GameManager.Px = (transform.position.x + transform.localScale.x / 2.0f + (n / 2.0f));
             GameManager.Py = (transform.position.y + transform.localScale.y / 2.0f + (m / 2.0f));
         }
 
+        rb.velocity = change * Speed;
 
-        rb.velocity = change * speed;
-
-        if(pistolCoolDown > 0.0f) {
-            pistolCoolDown = Mathf.Max(pistolCoolDown - Time.deltaTime, 0.0f);
+        if(_pistolCooldown > 0.0f) {
+            _pistolCooldown = Mathf.Max(_pistolCooldown - Time.deltaTime, 0.0f);
         }
-        if(automaticCoolDown > 0.0f) {
-            automaticCoolDown = Mathf.Max(automaticCoolDown - Time.deltaTime, 0.0f);
+        if(_automaticCooldown > 0.0f) {
+            _automaticCooldown = Mathf.Max(_automaticCooldown - Time.deltaTime, 0.0f);
         }
-        if(shotgunCoolDown > 0.0f) {
-            shotgunCoolDown = Mathf.Max(shotgunCoolDown - Time.deltaTime, 0.0f);
+        if(_shotgunCooldown > 0.0f) {
+            _shotgunCooldown = Mathf.Max(_shotgunCooldown - Time.deltaTime, 0.0f);
         }
     }
 }
